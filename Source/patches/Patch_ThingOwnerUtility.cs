946fe334098bdc41678902ddaa6b092612eb380e
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2023-05-16 00:47:38 +0300

    TSS with time freeze add-on: be more alike to cryptosleep casket

--- Source/patches/Patch_ThingOwnerUtility.cs
+++ Source/patches/Patch_ThingOwnerUtility.cs
@@ -9,16 +9,27 @@ using Verse.AI;
 
 namespace zed_0xff.CPS;
 
-// set comfort temperature for pawns/things inside TSS
+// TSS: set comfort temperature for pawns/things inside
 [HarmonyPatch(typeof(ThingOwnerUtility), nameof(ThingOwnerUtility.TryGetFixedTemperature))]
 static class Patch_TryGetFixedTemperature
 {
-    static bool Prefix( ref bool __result, ref float temperature, IThingHolder holder, Thing forThing){
-        if( holder is Building_TSS b && b.PowerOn ){
+    static bool Prefix( ref bool __result, ref float temperature, IThingHolder holder, Thing forThing ){
+        if( holder is Building_TSS tss && tss.PowerOn ){
             temperature = ( forThing is Pawn ) ? 21f : 0f;
             __result = true;
             return false;
         }
         return true;
+    }
+}
+
+// TSS: mark contents as in cryptosleep if time is freezed
+[HarmonyPatch(typeof(ThingOwnerUtility), nameof(ThingOwnerUtility.ContentsInCryptosleep))]
+static class Patch_ContentsInCryptosleep
+{
+    static void Postfix( ref bool __result, IThingHolder holder ){
+        if( holder is Building_TSS tss && tss.IsContentsSuspended ){
+            __result = true;
+        }
     }
 }
